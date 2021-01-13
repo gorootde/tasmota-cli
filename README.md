de609adda176533e9fb1e48920dec9d4d2f30281
Michael Kolb <dev@db1smk.com>
2021-01-13 20:18:14 +0100

    Added backup & legacy auth functionality

--- README.md
+++ README.md
@@ -3,10 +3,29 @@ CLI utility to manage tasmota devices from the command line.
 
 ![CI](https://github.com/gorootde/tasmota-cli/workflows/CI/badge.svg) [![Open Source Love](https://badges.frapsoft.com/os/gpl/gpl.svg?v=102)](https://github.com/ellerbrock/open-source-badge/)
 
+# Usage
+```bash
+Usage: tasmota-cli [OPTIONS] <command> <ip> (<ip> <ip> ...)
+
+  <command> 	Any tasmota command. See https://tasmota.github.io/docs/Commands/
+  <ip> 		List of the IPs of the tasmota devices to execute the command on
+
+CLI Arguments always take precedence over environment eariables
+  TASMOTACLI_USERNAME  Username
+  TASMOTACLI_PASSWORD  Password
+
+Flags:
+  -la
+    	Enable legacy authentication mode (for tasmota versions <= 9.2.0)
+  -p string
+    	The password used to authenticate to tasmota
+  -u string
+    	The username used to authenticate to tasmota
+  -v	Enable verbose mode
+```
 
 # Features
 - Execute [commands](https://tasmota.github.io/docs/Commands/) on one or more tasmota devices with a single CLI call
 
 ** Coming soon **
 - Scan for Tasmota devices in your network (Discovery)
-- Create automated backups
--- tasmota-cli/main.go
+++ tasmota-cli/main.go
@@ -4,39 +4,106 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
 var verbose bool
+var legacyAuth bool
 
 func verbosePrintln(format string, a ...interface{}) {
 	if verbose {
-		fmt.Printf(format, a)
+		fmt.Printf(format, a...)
 		fmt.Printf("\n")
 	}
 }
 
 func printUsage() {
-	fmt.Printf("Usage: %s [OPTIONS] <command> <ip> (<ip> <ip> ...)\n", os.Args[0])
+	fmt.Printf("Usage: %s [OPTIONS] <command> <ip> (<ip> <ip> ...)\n\n", os.Args[0])
+	fmt.Println("  <command> 	Any tasmota command. See https://tasmota.github.io/docs/Commands/")
+	fmt.Println("  <ip> 		List of the IPs of the tasmota devices to execute the command on")
+	fmt.Println("\nCLI Arguments always take precedence over environment eariables")
+	fmt.Println("  TASMOTACLI_USERNAME  Username")
+	fmt.Println("  TASMOTACLI_PASSWORD  Password")
+	fmt.Println("\nFlags:")
 	flag.PrintDefaults()
 }
 
+func downloadBackup(ip string, user string, password string) (string, error) {
+
+	url := fmt.Sprintf("http://%s/dl?", ip)
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	addAuthentication(req, user, password)
+
+	filename := fmt.Sprintf("tasmota_backup_%s.bin", ip)
+	fp, err := os.Create(filename)
+	if err != nil {
+		return "", err
+	}
+	defer fp.Close()
+
+	resp, err := performRequest(req)
+	if err != nil {
+		verbosePrintln("Error: %s", err)
+		return "", err
+	}
+
+	if resp.StatusCode != 200 {
+		body, _ := ioutil.ReadAll(resp.Body)
+		verbosePrintln("Response: %d %s", resp.StatusCode, string(body))
+		return string(body), nil
+	}
+	defer resp.Body.Close()
+	_, err = io.Copy(fp, resp.Body)
+
+	return "OK", nil
+}
+
+func performRequest(req *http.Request) (*http.Response, error) {
+	client := &http.Client{
+		Timeout: 1000 * time.Millisecond,
+	}
+	return client.Do(req)
+
+}
+
+func addAuthentication(req *http.Request, user string, password string) {
+	//Authentication
+	if len(password) > 0 || len(user) > 0 {
+		if legacyAuth {
+			q := req.URL.Query()
+			q.Add("user", user)         //required for Tasmota <= v9.2.0
+			q.Add("password", password) //required for Tasmota <= v9.2.0
+			req.URL.RawQuery = q.Encode()
+		} else {
+			req.SetBasicAuth(user, password)
+		}
+	}
+}
+
 func sendCommand(ip string, command string, user string, password string) (string, error) {
-	client := &http.Client{}
 	url := fmt.Sprintf("http://%s/cm", ip)
+
+	client := &http.Client{
+		Timeout: 1000 * time.Millisecond,
+	}
 
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("Accept", "application/json")
-	if len(password) > 0 && len(user) > 0 {
-		req.SetBasicAuth(user, password)
-	}
 
+	addAuthentication(req, user, password)
+
+	//Create Querystring
 	q := req.URL.Query()
 	q.Add("cmnd", command)
 	req.URL.RawQuery = q.Encode()
 
+	//Perform request
 	resp, err := client.Do(req)
 	if err != nil {
 		verbosePrintln("Error: %s", err)
@@ -46,19 +113,25 @@ func sendCommand(ip string, command string, user string, password string) (strin
 	body, _ := ioutil.ReadAll(resp.Body)
 	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
 		verbosePrintln("Response: %s", string(body))
-		var parsed map[string]map[string]interface{}
-		json.Unmarshal([]byte(body), &parsed)
-
-		return parsed["StatusFWR"]["Version"].(string), nil
+		return string(body), nil
 	}
 	return "", fmt.Errorf("HTTP %d. %s", resp.StatusCode, body)
+}
 
+func parseFwVersion(response string) string {
+	var parsed map[string]map[string]interface{}
+	json.Unmarshal([]byte(response), &parsed)
+	return parsed["StatusFWR"]["Version"].(string)
 }
 
 func main() {
 	flag.Usage = printUsage
-	user := flag.String("u", "", "The username used to authenticate to tasmota")
-	password := flag.String("p", "", "The password used to authenticate to tasmota")
+	var user string
+	var password string
+
+	flag.StringVar(&user, "u", os.Getenv("TASMOTACLI_USERNAME"), "The username used to authenticate to tasmota")
+	flag.StringVar(&password, "p", os.Getenv("TASMOTACLI_PASSWORD"), "The password used to authenticate to tasmota")
+	flag.BoolVar(&legacyAuth, "la", false, "Enable legacy authentication mode (for tasmota versions <= 9.2.0)")
 	flag.BoolVar(&verbose, "v", false, "Enable verbose mode")
 	flag.Parse()
 
@@ -75,7 +148,14 @@ func main() {
 		var error error
 		switch command {
 		case "version":
-			result, error = sendCommand(ip, "Status 2", *user, *password)
+			result, error = sendCommand(ip, "Status 2", user, password)
+			if error == nil {
+				result = parseFwVersion(result)
+			}
+		case "backup":
+			result, error = downloadBackup(ip, user, password)
+		default:
+			result, error = sendCommand(ip, command, user, password)
 		}
 
 		if error == nil {
